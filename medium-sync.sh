6a3a7e580e53758562a4c8d9039ab13d8352cbce
Matej Marconak <2461939+marconak-matej@users.noreply.github.com>
2025-11-20 18:44:26 +0100

    chore: improve medium-sync script

--- medium-sync.sh
+++ medium-sync.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 bundle update && bundle install
-curl https://medium.com/feed/@marconak-matej > .rss.xml
+
+ruby -ropen-uri -e "File.write('.rss.xml', URI.open('https://medium.com/feed/@marconak-matej').read)"
 bundle exec jekyll import rss --source .rss.xml --canonical_link true --extract_tags category
